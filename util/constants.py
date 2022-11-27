a5db60727fd8979536746d6d785f63e45d8d3e22
Osman Aziz <osmanaziz012@gmail.com>
2022-11-27 21:44:52 +0500

    scraping module fixed

--- util/constants.py
+++ util/constants.py
@@ -3,7 +3,10 @@
 LOCAL="http://localhost:81/data.html"
 
 # filepath var contain address of a file in which all url to be placed
-FILEPATH="url.txt"
+FILEPATH="../url.txt"
 
 # From what year images should be fetched 
 INITIAL_DATE=2005
+
+# Downlaod Images and save into this file/folder
+IMAGEFILEPATH='../data/without Labels/'
--- util/scrape.py
+++ util/scrape.py
@@ -6,18 +6,24 @@
 import requests
 from bs4 import  BeautifulSoup
 import constants 
+from googletrans import Translator
+import os
 
+translater=Translator()
+            
 class Scrape:
     baseURL=constants.LOCAL
     filePath=constants.FILEPATH
     fileInstanse=None
+    imageFilePath=constants.IMAGEFILEPATH
+    HEADERS=None
 
     def __init__(self):
-        self.fileInstanse= open(self.filePath, "w")
-    
-    def getHTML(self):
+        pass
+
+    def getHTML(self,url):
         try:
-            r=requests.get(self.baseURL)
+            r=requests.get(url,headers=self.HEADERS)
             soup = BeautifulSoup(r.content, 'lxml')
             return soup
         except:
@@ -25,6 +31,7 @@ class Scrape:
 
     def dumpIDs(self):
         try:
+            self.openFile(self.filePath,'a')
             print("****** Start Dumping URL into file ******")
             HTML=self.getHTML()
             rows=HTML.find('table').findAll('tr')
@@ -48,6 +55,68 @@ class Scrape:
             print(f"Error: e")
             return 0
 
+    def downloadAndDump(self,images,baseURL):
+        print(f"Start Downloading...") 
+        u=baseURL.replace('\n','')
+        HEADERS=({"Referer": f"{u}&gc=gc"})
+        for image in images:
+            print(f"Headers: {HEADERS}")
+            try:
+                HTML=requests.get(image,headers=HEADERS)
+                lst = os.listdir(self.imageFilePath) 
+                self.openFile(f"{self.imageFilePath}{len(lst)}.jpg",'wb')
+                self.fileInstanse.write(HTML.content)
+            except Exception as e:
+                 print(f"Fail to download: {image}") 
+                 print(f"Error: {e}") 
+                 continue
+        print(f"End Downloading\n\n")
+        
+    
+    def openFile(self,path,mode):
+        self.fileInstanse=open(path,mode)
 
-    def downloadImages(self):
-        pass
+
+    # Get URL from url.txt file then retreive HTML content which contains images of respective URL
+    def fetchImages(self):
+        self.openFile(self.filePath,'r')
+        lines=self.fileInstanse.readlines()
+        for url in lines:
+            imgSrcArr=[]
+            container=None
+            print(f"\n{url}")
+            try:
+                HTML=self.getHTML(f"{url}&gc=gc")    
+                check=HTML.findAll('div',class_='tabletitle')  
+                for i in check: 
+                    trans=translater.translate(i.text,dest="en").text
+                    if trans == ' character':
+                        table=i.findNext()
+                        container=table.findAll('tr')
+                    else:
+                        continue
+            except Exception as e:
+                print(f"Failed to fetch. Check Error.txt file")
+                open('../error.txt', 'a').write(url)
+                continue
+           
+           
+            try:
+                for item in container:
+                    if item.find('img'):
+                        imgSrcArr.append(f"http://www.getchu.com{item.find('img')['src'][1:]}")
+            except:
+                print(f"No image to fetch. Check 404.txt file")
+                open('../404.txt', 'a').write(url)
+                continue
+            print(f"\n imgSrcArr Length: {len(imgSrcArr)}") 
+            if len(imgSrcArr) > 0:    
+                self.downloadAndDump(imgSrcArr,url)   
+        print("\n\n Process Ends")   
+
+    def driver(self):
+        print(f"======= Starting Process ========\n")
+        self.fetchImages()
+
+
+Scrape().driver()
