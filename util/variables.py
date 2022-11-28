9bcd42ae6268cec4edd1b46c122b65f8cc38e820
Osman Aziz <osmanaziz012@gmail.com>
2022-11-28 20:23:16 +0500

    complete till 578685

--- util/variables.py
+++ util/variables.py
@@ -3,14 +3,14 @@
 LOCAL="http://localhost:81/data.html"
 
 # filepath var contain address of a file in which all url to be placed
-FILEPATH="../url.txt"
+FILEPATH="url.txt"
 
 # From what year images should be fetched 
 INITIAL_DATE=2005
 
 # Downlaod Images and save into this file/folder
-IMAGEFILEPATH='../data/images/'
+IMAGEFILEPATH='data/labeled/'
 
 # below var contain path of files which is used to track url failed to scrape images.
-ERROR404='../errors/404.txt'
-ERROR403='../errors/403.txt'
+ERROR404='errors/404.txt'
+ERROR403='errors/403.txt'
